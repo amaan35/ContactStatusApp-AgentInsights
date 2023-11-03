34ef45ed6a614123d37dcd35dca5b308fb936b9d
amaan35 <amaanlm35@gmail.com>
2023-11-03 16:40:43 +0530

    Modifying Broadcast Receiver class and overriding onReceive method, getting Shared Preferences and boolean values stored in it to display toast message

--- app/src/main/java/com/example/contactstatusapp/CallReceiver.java
+++ app/src/main/java/com/example/contactstatusapp/CallReceiver.java
@@ -1,4 +1,22 @@
 package com.example.contactstatusapp;
 
-public class CallReceiver {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.telephony.TelephonyManager;
+import android.widget.Toast;
+
+public class CallReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if(intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_RINGING)){
+            SharedPreferences sharedPreferences = context.getSharedPreferences("STATUS_HANDLER", Context.MODE_PRIVATE);
+            boolean isOn = sharedPreferences.getBoolean("isOn", false);
+            if(isOn){
+                String status = sharedPreferences.getString("status", "");
+                Toast.makeText(context, status, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 }

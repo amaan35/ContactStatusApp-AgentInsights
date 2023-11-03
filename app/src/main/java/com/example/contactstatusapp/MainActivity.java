f4237e183e73ff5386a4a7f799ca655d53548edf
amaan35 <amaanlm35@gmail.com>
2023-11-03 16:55:47 +0530

    Adding functionality to the toggle button, handling response of user regarding permissions

--- app/src/main/java/com/example/contactstatusapp/MainActivity.java
+++ app/src/main/java/com/example/contactstatusapp/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.contactstatusapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.material.switchmaterial.SwitchMaterial;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int PERMISSION_REQUEST_CODE = 200;
+    CallReceiver receiver = new CallReceiver();
+    SwitchMaterial toggle;
+    private boolean isReceiverRegistered = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +44,45 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Status saved, now toggle on to apply", Toast.LENGTH_SHORT).show();
             }
         });
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b && ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{android.Manifest.permission.READ_PHONE_STATE}, PERMISSION_REQUEST_CODE);
+                } else if (b && ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
+                    SharedPreferences sharedPreferences = getSharedPreferences("STATUS_HANDLER", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean("isOn", true);
+                    editor.apply();
+                    registerReceiver(receiver, new IntentFilter(TelephonyManager.ACTION_PHONE_STATE_CHANGED));
+                    isReceiverRegistered = true;
+                } else {
+                    if (isReceiverRegistered) {
+                        isReceiverRegistered = false;
+                        unregisterReceiver(receiver);
+                    }
+                }
+            }
+        });
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                SharedPreferences sharedPreferences = getSharedPreferences("STATUS_HANDLER", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putBoolean("isOn", true);
+                editor.apply();
+                registerReceiver(receiver, new IntentFilter(TelephonyManager.ACTION_PHONE_STATE_CHANGED));
+                isReceiverRegistered = true;
+            } else {
+                toggle.setChecked(false);
+                if (isReceiverRegistered) {
+                    isReceiverRegistered = false;
+                    unregisterReceiver(receiver);
+                }
+            }
+        }
     }
 }

e0708f3e917b10d8424ea468cd452f55b8c8ec7c
amaan35 <amaanlm35@gmail.com>
2023-11-03 16:09:05 +0530

    MainActivity modified - Added Custom Status EditText, Save Status Button and Shared Preferences

--- app/src/main/java/com/example/contactstatusapp/MainActivity.java
+++ app/src/main/java/com/example/contactstatusapp/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.contactstatusapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.switchmaterial.SwitchMaterial;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText statusEDT = findViewById(R.id.statusEdittext);
+        Button saveButton = findViewById(R.id.saveButton);
+        SwitchMaterial toggle = findViewById(R.id.switchBtn);
+        SharedPreferences sharedPreferences = getSharedPreferences("STATUS_HANDLER", MODE_PRIVATE);
+        String customStatus = sharedPreferences.getString("status", "");
+        statusEDT.setText(customStatus);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String newStatus = statusEDT.getText().toString();
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("status", newStatus);
+                editor.apply();
+                Toast.makeText(MainActivity.this, "Status saved, now toggle on to apply", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
